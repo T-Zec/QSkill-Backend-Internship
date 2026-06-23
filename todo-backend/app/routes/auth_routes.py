49c027282c51c81cdeaff1e8f8710e75f45603e9
T-Zec <sagarsp8920@gmail.com>
2026-06-23 23:14:57 +0530

    Implement access_token based JWT Authentication system and login and me endpoints

--- todo-backend/app/routes/auth_routes.py
+++ todo-backend/app/routes/auth_routes.py
@@ -1,14 +1,24 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from app.services.auth_service import AuthService
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 
-@auth_bp.route('/me', methods=['GET'])
-def me():
-    return {
-        "message": "Protected route works"
-    }
-
 @auth_bp.route("/register", methods=['POST'])
 def register():
     return AuthService.register()
+
+@auth_bp.route("/login", methods=['POST'])
+def login():
+    return AuthService.login()
+
+@auth_bp.route('/me', methods=['GET'])
+@jwt_required()
+def me():
+    current_user = get_jwt_identity()
+
+    return {
+        "success": True,
+        "user_id": current_user
+    }, 200
--- todo-backend/app/services/auth_service.py
+++ todo-backend/app/services/auth_service.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 
 from app.extension import db
 from app.models import User
@@ -19,6 +20,24 @@ class AuthService:
             return jsonify({
                 "success": False,
                 "message": "All fields are required"
+            }), 400
+        
+        if len(username) > 100:
+            return jsonify({
+                "success": False,
+                "message": "Username must be within 100 characters"
+            }), 400
+        
+        if len(email) > 255:
+            return jsonify({
+                "success": False,
+                "message": "Email must be within 255 characters"
+            }), 400
+        
+        if len(password) > 255:
+            return jsonify({
+                "success": False,
+                "message": "Password must be within 255 characters"
             }), 400
         
         existing_user = User.query.filter(
@@ -42,3 +61,38 @@ class AuthService:
             "success": True,
             "message": "User registered successfully"
         }), 201
+
+    @staticmethod
+    def login():
+        data = request.get_json()
+
+        email = data.get("email")
+        password = data.get("password")
+
+        if not email or not password:
+            return jsonify({
+                "success": False,
+                "message": "Email and password are requried"
+            }), 400
+        
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return jsonify({
+                "success": False,
+                "message": "Invalid credentials"
+            }), 401
+        
+        if not check_password_hash(user.password_hash, password):
+            return jsonify({
+                "success": False,
+                "message": "Invalid credentials"
+            }), 401
+        
+        access_token = create_access_token(identity=str(user.id))
+
+        return jsonify({
+            "success": True,
+            "message": "Login successful",
+            "access_token": access_token
+        }), 200
